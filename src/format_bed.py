28ce6ff125234c0798cd4d46d1b00a2e67d920bc
hardva <Nassim23456@gmail.com>
2022-10-15 00:04:14 +0200

    Forgot this assignment to last sec...

--- src/format_bed.py
+++ src/format_bed.py
@@ -30,6 +30,8 @@ def main() -> None:
     # With all the options handled, we just need to do the real work
     # FIXME: put your code here
 
+for line in args.infile:
+    print_line(parse_line(line), args.outfile)
 
 if __name__ == '__main__':
     main()
--- src/query_bed.py
+++ src/query_bed.py
@@ -29,6 +29,21 @@ def main() -> None:
     # With all the options handled, we just need to do the real work
     # FIXME: put your code here
 
+parsed_file=Table()
+for line in args.bed:
+    parsed_line=parse_line(line)
+    parsed_file.add_line(parsed_line)
+
+for line in args.query.redlines():
+    tab_seperation = line.split("\t")
+    All_lines_chrom = parsed_file.get_chrom(tab_seperation[0])
+    start = int(tab_seperation[1])
+    end = int(tab_seperation[2])
+
+    for line in All_lines_chrom:
+        if start <= line.chrom_start < end:
+            print_line(k, args.outfile)
+    
 
 if __name__ == '__main__':
     main()
